0cf224c1ec7c23edecb7c517a81d10750f01ed65
zorojuro0101 <symonkiel01@gmail.com>
2026-08-07 10:48:52 +0800

    fixed file structure

---
+++ js/main.js
@@ -0,0 +1,18 @@
+(function () {
+  "use strict";
+
+  var navToggle = document.querySelector(".nav-toggle");
+  var siteNav = document.querySelector(".site-nav");
+
+  if (navToggle && siteNav) {
+    navToggle.addEventListener("click", function () {
+      var expanded = siteNav.classList.toggle("open");
+      navToggle.setAttribute("aria-expanded", String(expanded));
+    });
+  }
+
+  var yearEl = document.getElementById("year");
+  if (yearEl) {
+    yearEl.textContent = String(new Date().getFullYear());
+  }
+})();
